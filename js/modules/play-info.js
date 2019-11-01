1fb86c4b14506b85087b5ece9bca3895b585d523
szantilas87 <szantilas@gmail.com>
2019-11-01 13:49:17 +0000

    Basic player add playlist

---
+++ js/modules/play-info.js
@@ -0,0 +1,43 @@
+import Playlist from './playlist.js'
+
+const PlayInfo = (_ => {
+
+    const state = {
+        songsLength: 0,
+        isPlaying: false
+    }
+
+    //cache the DOM
+    const playerCountEl = document.querySelector(".player__count");
+    const playerTriggerEl = document.querySelector(".player__trigger");
+
+
+    const init = _ => {
+        render();
+        listeners();
+    }
+
+    const listeners = _ => {
+        playerTriggerEl.addEventListener("click", _ => {
+            state.isPlaying = state.isPlaying ? false : true;
+            render();
+            Playlist.flip();
+        })
+    }
+    const setState = obj => {
+        state.songsLength = obj.songsLength;
+        state.isPlaying = obj.isPlaying;
+        render();
+    }
+
+    const render = _ => {
+        playerCountEl.innerHTML = state.songsLength;
+        playerTriggerEl.innerHTML = state.isPlaying ? "Pause" : "Play";
+    }
+    return {
+        init,
+        setState
+    }
+})();
+
+export default PlayInfo;
---
+++ js/modules/track-bar.js
@@ -0,0 +1,36 @@
+const TrackBar = (_ => {
+
+    const state = {
+        currentTrackTime: 0,
+        fullTrackTime: 0,
+        fillWidth: 0
+    }
+
+    const trackBarFillEl = document.querySelector(".track-bar__fill");
+
+    const init = _ => {
+        render();
+    }
+
+    const render = _ => {
+        trackBarFillEl.style.width = `${state.fillWidth}%`
+    }
+
+    const getPercent = (current, full) => {
+        return (current / full) * 100;
+    }
+
+    const setState = obj => {
+        state.currentTrackTime = obj.currentTime;
+        state.fullTrackTime = obj.duration;
+        state.fillWidth = getPercent(state.currentTrackTime, state.fullTrackTime);
+        render();
+    }
+
+    return {
+        init,
+        setState
+    }
+})();
+
+export default TrackBar;
